5766f34d24b0fef312b893a97a2ca9edac96f0bd
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-02-20 01:16:57 +0800

    Add refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
 LOW_THRESHOLD = 5
 HIGH_THRESHOLD = 10
 
-password = input("Enter password")
-while len(password) < LOW_THRESHOLD or len(password) > HIGH_THRESHOLD:
-    print("Invalid password!")
-    password = input("Enter password")
-print(f"{'*'} * {len(password)}")
+
+def main():
+    password = get_password()
+    method_name(password)
+
+
+def method_name(password):
+    print(f"{'*'} * {len(password)}")
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < LOW_THRESHOLD or len(password) > HIGH_THRESHOLD:
+        print("Invalid password!")
+        password = input("Enter password: ")
+    return password
+
+
+main()
