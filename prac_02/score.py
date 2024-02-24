601c2799d22756a564c4f2523adcdc58ceb689f4
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-02-25 01:56:12 +0800

    Refactor score program to use functions

---
+++ prac_02/score.py
@@ -0,0 +1,17 @@
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score_category(score)
+    print(result)
+
+
+def determine_score_category(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    return "Excellent"
+
+
+main()
