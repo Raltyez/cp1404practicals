92bbb375d301d4e6b0419a67c24c39a1c9b08937
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-03-09 20:34:39 +0800

    Add actual finished exercise

--- prac_04/list_exercises..py
+++ prac_04/list_exercises..py
@@ -1,13 +1,24 @@
+# Basic list operations
 numbers = []
 total = 0
 for i in range(0, 5):
     number = int(input("Number: "))
     numbers.append(number)
-    total += number
-average = total/5
+
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[4]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {average}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
 
