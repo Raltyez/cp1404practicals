af87f2c1cc7df015518a995fa1f3ed51ddbef629
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-03-17 02:06:28 +0800

    Complete prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,14 @@
+
+text = input("Text: ")
+words = text.split()
+texts_to_count = {}
+for word in words:
+    if word not in texts_to_count:
+        texts_to_count[word] = 1
+    elif word in texts_to_count:
+        texts_to_count[word] += 1
+text_counts = texts_to_count.items()
+sorted_text_counts = sorted(text_counts)
+for piece in sorted_text_counts:
+    print(f"{piece[0]:10} : {piece[1]}")
+
