3949cc848cd77d08a4f73c87260ffd1cb6f5f4cc
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-03-17 19:21:09 +0800

    Complete prac 5 emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,10 +2,11 @@ SPECIAL_CHARACTERS = '.,-_!'
 
 
 def main():
+    """Create dictionary of emails to names"""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        name = extract_name(email)
+        name = extract_name_from_email(email)
         choice = input(f"Is your name {name.title()}? (Y/n) ").lower()
         if choice != "y" and choice != "":
             name = input("Name: ").title()
@@ -16,7 +17,8 @@ def main():
         print(f"{name} ({email})")
 
 
-def extract_name(email):
+def extract_name_from_email(email):
+    """Extract name from email address"""
     name = email.split('@')[0]
     for check in range(len(SPECIAL_CHARACTERS)):
         if SPECIAL_CHARACTERS[check] in name:
