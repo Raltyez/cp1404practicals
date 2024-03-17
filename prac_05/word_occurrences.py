4a701305b2f17447248471152c721f74382da8d9
Raltyez <ralph.jirehelbudiono@my.jcu.edu.au>
2024-03-17 20:23:52 +0800

    Complete prac 5 word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,10 +3,15 @@ text = input("Text: ")
 words = text.split()
 texts_to_count = {}
 for word in words:
-    if word not in texts_to_count:
-        texts_to_count[word] = 1
-    elif word in texts_to_count:
+    # LBYL method (which works)
+    # if word not in texts_to_count:
+    #     texts_to_count[word] = 1
+    # elif word in texts_to_count:
+    #     texts_to_count[word] += 1
+    try:
         texts_to_count[word] += 1
+    except KeyError:
+        texts_to_count[word] = 1
 text_counts = texts_to_count.items()
 sorted_text_counts = sorted(text_counts)
 for piece in sorted_text_counts:
